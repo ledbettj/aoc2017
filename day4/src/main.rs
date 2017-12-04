8815d99bb496e58b28aa3a7d54087cd01700444f
John Ledbetter <john@callrail.com>
2017-12-04 09:18:13 -0500

    day4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -17,6 +17,25 @@ fn valid_passphrase(phrase: &str) -> bool {
   true
 }
 
+fn valid_passphrase_no_angrams(phrase: &str) -> bool {
+  let mut set = HashSet::new();
+
+  for word in phrase.split(" ") {
+    let mut chars : Vec<char> = word.chars().collect();
+    chars.sort();
+
+    let sorted : String = chars.iter().collect();
+    if set.contains(&sorted) {
+      return false;
+    }
+
+    set.insert(sorted);
+  }
+
+  true
+}
+
+
 fn main() {
   let args = env::args();
   let filename = args.skip(1).last().expect("You must provide a filename");
@@ -26,9 +45,11 @@ fn main() {
     Ok(mut file) => {
       file.read_to_string(&mut contents);
       let lines = contents.lines();
-      let valid = lines.filter(|phrase| valid_passphrase(phrase)).count();
+      //let valid = lines.filter(|phrase| valid_passphrase(phrase)).count();
+      let valid_angrams = lines.filter(|phrase| valid_passphrase_no_angrams(phrase)).count();
 
-      println!("{} passphrases are valid\n", valid);
+      //println!("{} passphrases are valid for part 1\n", valid);
+      println!("{} passphrases are valid for part 2\n", valid_angrams);
     }
 
     Err(e) => panic!(e)
