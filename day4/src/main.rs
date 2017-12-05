0fb6e9ac104f1cb047870cbd846574ad98aaa102
John Ledbetter <john.ledbetter@gmail.com>
2017-12-04 22:19:23 -0500

    simplify day4 a bit

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,56 +3,42 @@ use std::fs::File;
 use std::io::*;
 use std::collections::HashSet;
 
-fn valid_passphrase(phrase: &str) -> bool {
+fn is_valid(phrase: &str) -> bool {
   let mut set = HashSet::new();
 
-  for word in phrase.split(" ") {
-    if set.contains(word) {
-      return false;
-    }
-
-    set.insert(word);
-  }
-
-  true
+  phrase.split(" ").all(|word| set.insert(word))
 }
 
-fn valid_passphrase_no_angrams(phrase: &str) -> bool {
+fn is_valid_no_anagrams(phrase: &str) -> bool {
   let mut set = HashSet::new();
 
-  for word in phrase.split(" ") {
+  phrase.split(" ").all(|word| {
     let mut chars : Vec<char> = word.chars().collect();
     chars.sort();
 
     let sorted : String = chars.iter().collect();
-    if set.contains(&sorted) {
-      return false;
-    }
-
-    set.insert(sorted);
-  }
-
-  true
+    set.insert(sorted)
+  })
 }
 
 
 fn main() {
-  let args = env::args();
-  let filename = args.skip(1).last().expect("You must provide a filename");
+  let filename = env::args().skip(1).last().expect("You must provide a filename");
+
+  let mut file = File::open(filename).expect("Failed to open file");
   let mut contents = String::new();
 
-  match File::open(filename) {
-    Ok(mut file) => {
-      file.read_to_string(&mut contents);
-      let lines = contents.lines();
-      //let valid = lines.filter(|phrase| valid_passphrase(phrase)).count();
-      let valid_angrams = lines.filter(|phrase| valid_passphrase_no_angrams(phrase)).count();
+  file.read_to_string(&mut contents).expect("Failed to read file");
 
-      //println!("{} passphrases are valid for part 1\n", valid);
-      println!("{} passphrases are valid for part 2\n", valid_angrams);
+  let lines = contents.lines();
+
+  let (valid, valid_anagrams) = lines.fold((0, 0), |(v, va), phrase| {
+    match (is_valid(phrase), is_valid_no_anagrams(phrase)) {
+      (true, true)  => (v + 1, va + 1),
+      (true, false) => (v + 1, va),
+      _ => (v, va)
     }
+  });
 
-    Err(e) => panic!(e)
-  }
-  
+  println!("part 1: {}; part 2: {}", valid, valid_anagrams)
 }
