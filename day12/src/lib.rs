97e16ce5132194eca55e225388402af82cac81ed
John Ledbetter <john@callrail.com>
2017-12-12 09:23:36 -0500

    add day12p2

--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -36,6 +36,39 @@ mod day12 {
     results
   }
 
+  pub fn count_groups(graph: &mut HashMap<u32, HashSet<u32>>) -> u32 {
+    let mut count = 0;
+
+    while !graph.is_empty() {
+      let (&start, _) = graph.iter().last().unwrap();
+      remove_group(graph, start);
+      count += 1;
+    }
+
+    count
+  }
+
+  pub fn remove_group(graph: &mut HashMap<u32, HashSet<u32>>, start: u32) {
+    let mut seen : HashSet<u32> = HashSet::new();
+    let mut stack : Vec<u32> = vec![start];
+
+    while !stack.is_empty() {
+      let v = stack.pop().unwrap();
+      {
+        if let Some(neighbors) = graph.get(&v) {
+
+          for n in neighbors {
+            if seen.insert(*n) {
+              stack.push(*n);
+            }
+          }
+        }
+      }
+      graph.remove(&v);
+    }
+
+  }
+
   pub fn count_neighbors(graph: &HashMap<u32, HashSet<u32>>, start: u32) -> u32 {
     let mut seen : HashSet<u32> = HashSet::new();
     let mut stack = vec![start];
@@ -71,17 +104,21 @@ mod tests {
 5 <-> 6
 6 <-> 4, 5";
 
-    let data = parse_graph(input);
+    let mut data = parse_graph(input);
 
     assert_eq!(data.get(&2).unwrap().len(), 3);
 
     assert_eq!(count_neighbors(&data, 0), 6);
     assert_eq!(count_neighbors(&data, 1), 1);
+
+    assert_eq!(count_groups(&mut data), 2);
   }
 
   #[test]
   fn part1() {
-    let data = parse_graph(INPUT);
+    let mut data = parse_graph(INPUT);
     assert_eq!(count_neighbors(&data, 0), 113);
+
+    assert_eq!(count_groups(&mut data), 202);
   }
 }
